5a63e0e5de94880236f325ae0ba890cfffcb098c
CountyRipper <1097249217@qq.com>
2020-10-25 10:57:11 +0800

    dynamic

--- js/weight.js
+++ js/weight.js
@@ -14,39 +14,133 @@ function getinfo() {
 }
 
 
-echarts.init(document.getElementById('weight_chart')).setOption({
+var mychart = echarts.init(document.getElementById('weight_chart'));
+var option = {
     title: {
         left: 'center',
-        text: '重量感应',
+        text: '传感器',
     },
-    xAxis: {
+    // legend: {
+    //     data:['重量值', '气味值']
+    // },
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross',
+            label: {
+                backgroundColor: '#283b56'
+            }
+        }
+    },
+    toolbox: {
+        show: true,
+        feature: {
+            dataView: {readOnly: false},
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    xAxis: [{
         type: 'category',
-        data: ['17:40', '17:45', '17:50', '17:55', '18:00', '18:55', '18:10']
-    },
-    yAxis: {
-        type: 'value'
-    },
+        boundaryGap: true,
+        data: (function () {
+            var now = new Date();
+            var res = [];
+            var len = 10;
+            while (len--) {
+                res.unshift(now.toLocaleTimeString().replace(/^\D*/, ''));
+                now = new Date(now - 2000);
+            }
+            return res;
+        })()
+    }
+        // ,{
+        //     type: 'category',
+        //     boundaryGap: true,
+        //     data: (function (){
+        //         var res = [];
+        //         var len = 10;
+        //         while (len--) {
+        //             res.push(10 - len - 1);
+        //         }
+        //         return res;
+        //     })()
+        // }],
+    ],
+    yAxis: [
+        {
+            type: 'value',
+            scale: true,
+            name: '重量值',
+            max: 100,
+            min: 0,
+            boundaryGap: [0.2, 0.2]
+        },
+        {
+            type: 'value',
+            scale: true,
+            name: '气味值',
+            max: 200,
+            min: 0,
+            boundaryGap: [0.2, 0.2]
+        }
+    ],
     series: [{
-        data: [0.02, 0.02,3.25, 5.26, 5.36, 5.87,0.02 ],
+        name: "重量",
+        data: (function (){
+            var res = [];
+            var len = 10;
+            while (len--) {
+                res.push(0);
+            }
+            return res;
+        })()
+        ,
+        type: 'bar',
+        smooth: true
+    },
+    {
+        name: "气味值",
+        data: (function (){
+            var res = [];
+            var len = 0;
+            while (len < 10) {
+                res.push(0);
+                len++;
+            }
+            return res;
+        })(),
         type: 'line',
         smooth: true
-    }]
-});
-echarts.init(document.getElementById('odor_chart')).setOption({
-    title: {
-        left: 'center',
-        text: '气体感应',
-    },
-    xAxis: {
-        type: 'category',
-        data: ['17:40', '17:45', '17:50', '17:55', '18:00', '18:55', '18:10']
-    },
-    yAxis: {
-        type: 'value'
-    },
-    series: [{
-        data: [15, 16, 18, 25, 27, 22, 19],
-        type: 'line',
-        smooth: true
-    }]
-});
+    }
+    ]
+}
+
+mychart.setOption(option)
+//app.count = 11;
+setInterval(function () {
+
+    $.post("/getinfo", (data_sum, status) => {
+
+        //默认data_sum数据为json对象
+        console.log(data_sum)
+
+        //动态数据表
+        var axisData = (new Date()).toLocaleTimeString().replace(/^\D*/, '');
+        var data0 = option.series[0].data;
+        var data1 = option.series[1].data;
+        //设置重量
+        data0.shift()
+        data0.push(data_sum.weight)
+        //设置气味
+        data1.shift()
+        data1.push(data_sum.odor)
+        option.xAxis[0].data.shift()
+        option.xAxis[0].data.push(axisData)
+        mychart.setOption(option);
+    })
+
+    // option.xAxis[1].data.shift();
+    // option.xAxis[1].data.push(app.count++);
+
+}, 2100);
